e00b3a0c4616f0d7c54ce160b88a512dc90ba96d
MohakTakyar <156115409+MohakTakyar@users.noreply.github.com>
2024-03-02 08:26:40 -0800

    Initial Commit

---
+++ MyMidterm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMidterm
+//
+//  Created by user238229 on 3/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
